76b416ca87d530cb7e52f4e98498aa9d27f26ad7
AkcaMvlt <156558876+AkcaMvlt@users.noreply.github.com>
2024-03-14 22:18:30 +0100

    Add files via upload

--- assets/app-internacional.js
+++ assets/app-internacional.js
@@ -99,7 +99,7 @@ const channelsData = [
           <div id="video" class="video-container" style="--aspect-ratio: 16 / 9;">
               <iframe id="player" width="100%" src="${channel.source}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
-          <div class="channel-name"><h4>${channel.name}</h4><a target="_BLANK" href="${channel.source}"><span class="new-window">Ver en otra ventana</span><img width="16px" src="assets/img/Icon material-open-in-new.svg"></a></div>
+          <div class="channel-name"><h4>${channel.name}</h4><a target="_BLANK" href="${channel.source}"><span class="new-window"></span><img width="16px" src="assets/img/Icon material-open-in-new.svg"></a></div>
       </div>
     `;
   }
